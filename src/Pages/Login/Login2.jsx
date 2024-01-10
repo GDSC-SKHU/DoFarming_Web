a6d8769da78825e7845645b752727040c269a409
profitah <qwer120542@gmail.com>
2024-01-11 06:44:06 +0900

    Sign in to Google with Firebase

--- src/Pages/Login/Login2.jsx
+++ src/Pages/Login/Login2.jsx
@@ -1,7 +1,7 @@
 import "../../styles/login/Login.css";
 import { GoogleLoginButton } from "react-social-login-buttons";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { auth } from "../Firebase/firebase-config";
+import { auth } from "../../Firebase/firebase-config";
 import { useNavigate } from "react-router-dom"; 
 const Login2 = () => {
   const navigate = useNavigate(); 
